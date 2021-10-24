2a2900b660b6bb488867639c323eadd330080edf
xxbbxx825 <xxbbxx825@gmail.com>
2021-10-24 11:59:16 +0900

    create nuxt project

---
+++ nuxt/nuxt.config.js
@@ -0,0 +1,64 @@
+const environment = process.env.NODE_ENV || 'development'
+require('dotenv').config()
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxt',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy',
+    '@nuxtjs/dotenv',
+  ],
+
+  proxy: {
+    '/api':
+      environment === 'development'
+        ? process.env.API_URL
+        : 'https://www.example.org',
+  },
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    baseURL: process.env.API_URL,
+    browserBaseURL: process.env.API_BROWSER_URL,
+    credentials: true,
+    proxy: true,
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
---
+++ nuxt/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        {{ greet }}
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData({ app }): Promise<object> {
+    const greet: string = await app.$axios.$get('/').catch((err) => err)
+    return { greet }
+  },
+})
+</script>
