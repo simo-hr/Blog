923899f3011df6b49dfdcb66f08a08bf75f14ebe
xxbbxx825 <xxbbxx825@gmail.com>
2021-10-26 00:47:07 +0900

    add login/logout function #1

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MeController;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,11 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/admin/me', function (Request $request) {
+        return $request->user();
+    });
+});
 Route::get('/', function () {
     return 'Hello World';
 });
