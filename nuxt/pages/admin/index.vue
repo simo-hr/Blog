be5a07119b095def0209c18151d24b5de50bd180
xxbbxx825 <xxbbxx825@gmail.com>
2021-11-02 09:50:22 +0900

    make login page #13

---
+++ nuxt/pages/admin/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">admin index</h1>
+    </div>
+  </div>
+</template>
---
+++ nuxt/pages/admin/login.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="container mx-auto w-full max-w-md">
+    <form
+      class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      @submit.prevent="login"
+    >
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="email"
+          >Mail Address</label
+        >
+        <input
+          id="email"
+          v-model="form.email"
+          type="email"
+          class="
+            shadow
+            appearance-none
+            border
+            rounded
+            w-full
+            py-2
+            px-3
+            text-gray-700
+            leading-tight
+            focus:outline-none focus:shadow-outline
+          "
+        />
+      </div>
+      <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="password"
+          >Password</label
+        >
+        <input
+          id="password"
+          v-model="form.password"
+          type="password"
+          class="
+            shadow
+            appearance-none
+            border
+            rounded
+            w-full
+            py-2
+            px-3
+            text-gray-700
+            leading-tight
+            focus:outline-none focus:shadow-outline
+          "
+          autocomplete="off"
+        />
+      </div>
+      <div class="flex items-center justify-between">
+        <button
+          type="submit"
+          class="
+            bg-blue-500
+            hover:bg-blue-700
+            text-white
+            font-bold
+            py-2
+            px-4
+            rounded
+            focus:outline-none focus:shadow-outline
+          "
+        >
+          Login
+        </button>
+        <a
+          class="
+            inline-block
+            align-baseline
+            font-bold
+            text-sm text-blue-500
+            hover:text-blue-800
+          "
+          href="#"
+        >
+          Forgot Password?
+        </a>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: '',
+        password: '',
+      },
+    }
+  },
+  methods: {
+    login() {
+      this.$store.dispatch('auth/login', this.form)
+    },
+  },
+}
+</script>
