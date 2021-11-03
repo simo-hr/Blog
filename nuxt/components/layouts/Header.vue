dabbca8af6789369d2a16313214c15dc3cfa3e9d
xxbbxx825 <xxbbxx825@gmail.com>
2021-11-03 17:49:31 +0900

    make header #19

---
+++ nuxt/components/layouts/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div classs="container">
+    <header>
+      <nuxt-link to="/">
+        <div class="text-center align-middle p-5 mb-3 bg-red-800">
+          {{ siteName }}
+        </div>
+      </nuxt-link>
+      <Category :categories="categories" />
+    </header>
+  </div>
+</template>
+
+<script>
+import Category from '@/components/objects/category/Category'
+export default {
+  components: {
+    Category,
+  },
+  layout: 'Header',
+  props: {
+    categories: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  computed: {
+    siteName() {
+      return process.env.SITE_NAME
+    },
+  },
+}
+</script>
