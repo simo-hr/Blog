1a5ee06b82e8564cfc240ca1df279ea09d6d2150
xxbbxx825 <xxbbxx825@gmail.com>
2021-11-05 00:30:06 +0900

    WIP:make category id page #23

--- nuxt/pages/category/_id.vue
+++ nuxt/pages/category/_id.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="container">
-    <p>{{ $route.params.id }}</p>
+    <h2>{{ category.name }}</h2>
+    <div v-for="(article, index) in articles" :key="index">
+      {{ category.name }}
+      {{ article.title }}
+    </div>
   </div>
 </template>
 
 <script>
-import categoryMixin from '@/mixins/categoryMixins.js'
 export default {
-  mixins: [categoryMixin],
+  asyncData(app) {
+    return app.$axios
+      .get(`categories/${app.route.params.id}`)
+      .then((response) => {
+        return {
+          articles: response.data.articles,
+          category: response.data,
+        }
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  },
 }
 </script>
