216b60317bbbc47f237c3e4b1e052d2a907bba82
xxbbxx825 <xxbbxx825@gmail.com>
2021-11-04 20:02:14 +0900

    all pages exec getCategories process

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -7,26 +7,8 @@
 </template>
 
 <script>
+import categoryMixin from '@/mixins/categoryMixins.js'
 export default {
-  asyncData(app) {
-    return app.$axios
-      .get('categories')
-      .then((response) => {
-        return {
-          categories: response.data,
-        }
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  },
-  mounted() {
-    this.getCategories()
-  },
-  methods: {
-    getCategories() {
-      this.$nuxt.$emit('getCategories', this.categories)
-    },
-  },
+  mixins: [categoryMixin],
 }
 </script>
