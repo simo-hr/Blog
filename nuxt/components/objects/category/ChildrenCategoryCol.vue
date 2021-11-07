1aa3bd5b9fa1379a0309705f003ef4341ed1c15e
xxbbxx825 <xxbbxx825@gmail.com>
2021-11-07 14:39:01 +0900

    fix header layout #32

---
+++ nuxt/components/objects/category/ChildrenCategoryCol.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex flex-col w-full">
+    <div v-for="category in childrenCategories" :key="category.id">
+      <nuxt-link
+        class="
+          font-sans
+          inline-block
+          text-base
+          sm:text-lg
+          md:text-xl
+          lg:text-2xl
+          xl:text-3xl
+          align-middle
+          w-full
+          p-2
+          bg-white
+          hover:bg-gray-100
+        "
+        :to="{ path: '/category/' + category.id }"
+      >
+        &nbsp;&nbsp;&nbsp;・{{ category.name }}
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    childrenCategories: {
+      type: Array,
+      default: () => [],
+    },
+  },
+}
+</script>
