f49e0d5f86dbe4639fc0d92135bc2f50b9e19cb3
xxbbxx825 <xxbbxx825@gmail.com>
2021-11-18 22:04:58 +0900

    Make ArticleCaption more general #46

--- nuxt/pages/category/_id.vue
+++ nuxt/pages/category/_id.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="container mx-auto mb-10">
-    <h1
-      class="pl-3 mb-10 font-sans text-base sm:text-lg md:text-3xl lg:text-3xl xl:text-3xl"
-    >
-      {{ category.name }}
-    </h1>
+    <h1 class="h1">{{ category.name }}</h1>
     <p v-show="articles.length === 0" class="pl-5 h-">記事がありません</p>
-    <ArticleCaption :articles="articles" :category="category" />
+    <div class="flex flex-wrap">
+      <div
+        v-for="(article, index) in articles"
+        :key="index"
+        class="w-full sm:w-1/2 xl:w-1/3 p-2 mb-5"
+      >
+        <ArticleCaption :article="article">
+          {{ category.name }}
+        </ArticleCaption>
+      </div>
+    </div>
   </div>
 </template>
 
