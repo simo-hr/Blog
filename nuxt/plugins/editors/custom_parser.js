793b64cfafb8031de0d17591ef22661ac9e8b648
xxbbxx825 <xxbbxx825@gmail.com>
2021-12-01 22:12:12 +0900

    Fixed code-block layout #72

--- nuxt/plugins/editors/custom_parser.js
+++ nuxt/plugins/editors/custom_parser.js
@@ -1,10 +1,6 @@
 const codeParser = (block) => {
   return `
-    <pre id="code-block">
-			<code>
-				${block.data.code.replace(/</g, '&lt;').replace(/>/, '&gt;')}
-			</code>
-		</pre>
+    <pre id="code-block"><code>${block.data.code.replace(/</g, '&lt;').replace(/>/, '&gt;')}</code></pre>
 	`
 }
 
@@ -13,22 +9,14 @@ const listParser = (block) => {
   for (let i = 0; i < block.data.items.length; i++) {
     listItems += `<li>${block.data.items[i]}</li>`
   }
-  return `
-		<div id="order-list">
-			<ol>
-				${listItems}
-			</ol>
-		</div>
-	`
+  return `<div id="order-list"><ol>${listItems}</ol></div>`
 }
 
 const quoteParser = (block) => {
   return `
   <div id="quote-block">
     <div>
-        <blockquote cite="${block.data.caption}">
-            ${block.data.text}
-        </blockquote>
+        <blockquote cite="${block.data.caption}">${block.data.text}</blockquote>
         <span>${block.data.caption}</span>
     </div>
   </div>
