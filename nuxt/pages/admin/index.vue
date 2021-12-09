c65543831577978ecb375395a884445230376c6b
xxbbxx825 <xxbbxx825@gmail.com>
2021-12-09 20:28:26 +0900

    Fixed layouts related admin

--- nuxt/pages/admin/index.vue
+++ nuxt/pages/admin/index.vue
@@ -1,33 +1,11 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">admin index</h1>
-      <button
-        class="
-          bg-gray-900
-          hover:bg-gray-700
-          text-white
-          font-bold
-          py-2
-          px-4
-          h-full
-        "
-        @click="logout()"
-      >
-        Logout
-      </button>
-      <nuxt-link :to="{ path: '/article/create' }">新規投稿</nuxt-link>
-    </div>
+  <div class="w-full">
+    <nuxt-link :to="{ path: '/article/create' }">新規投稿</nuxt-link>
   </div>
 </template>
 
 <script>
 export default {
   middleware: ['checkIsAdmin'],
-  methods: {
-    logout() {
-      this.$store.dispatch('auth/logout')
-    },
-  },
 }
 </script>
